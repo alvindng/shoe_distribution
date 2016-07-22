164342eb9aaf90b335f7845d3679fa6b66dbe26b
Alvin Ng <alvindng@gmail.com>
2016-07-22 14:18:16 -0700

    add spec and pages for adding a store

--- app.rb
+++ app.rb
@@ -13,6 +13,27 @@ get('/stores') do
   erb(:stores)
 end
 
+post('/stores') do
+  store_name = params.fetch('store_name')
+  @store = Store.create({:name => store_name})
+  redirect('/stores')
+end
+
+get('/store/:id') do
+  @store = Store.find(params.fetch('id').to_i)
+  @brands = Brand.all()
+  erb(:store)
+end
+
+post('/store/:id/brand/new') do
+  brand_name = params.fetch('brand_name')
+  brand = Brand.create({:name => brand_name})
+  @store = Store.find(params.fetch('id').to_i)
+  @store.brands.push(brand)
+  redirect('/store/'.concat(@store.id().to_s()))
+end
+
 get('/brands') do
+  @brands = Brand.all()
   erb(:brands)
 end
--- spec/store_pages_spec.rb
+++ spec/store_pages_spec.rb
@@ -6,4 +6,11 @@ describe("the store user path", {:type => :feature}) do
     click_link('View All Stores')
     expect(page).to have_content('stores')
   end
+
+  it('lets you add a store') do
+    visit('/stores')
+    test_store = Store.create({:name => 'Champs'})
+    click_button('Add Store')
+    expect(page).to have_content('Champs')
+  end
 end
