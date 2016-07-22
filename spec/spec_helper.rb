98a8c8a1cbf54db5272facfb75cf85fc192c9840
Alvin Ng <alvindng@gmail.com>
2016-07-22 11:10:36 -0700

    add initial files and establish Brand and Store classes

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+ENV['RACK_ENV'] = 'test'
+
+require('bundler/setup')
+Bundler.require(:default, :test)
+set(:root, Dir.pwd())
+Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
