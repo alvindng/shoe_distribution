8ff068ee567619ca9fadfec3986fa5615fa1cd58
Alvin Ng <alvindng@gmail.com>
2016-07-22 11:15:54 -0700

    add tables for join, brands, and stores

---
+++ db/migrate/20160722181413_brands_stores.rb
@@ -0,0 +1,9 @@
+class BrandsStores < ActiveRecord::Migration
+  def change
+    create_table(:brands_stores) do |t|
+      t.column(:brand_id, :integer)
+      t.column(:store_id, :integer)
+      t.timestamps
+    end
+  end
+end
