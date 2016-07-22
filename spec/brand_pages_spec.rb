7238c3c9404b516edc87e864a8cc1c0cd16a5897
Alvin Ng <alvindng@gmail.com>
2016-07-22 15:37:00 -0700

    add specs for viewing all brands

--- spec/brand_pages_spec.rb
+++ spec/brand_pages_spec.rb
@@ -1,1 +1,9 @@
 require('spec_helper')
+
+describe("the brand user path", {:type => :feature}) do
+  it("shows all brands") do
+    visit('/')
+    click_link('View All Brands')
+    expect(page).to have_content('brands')
+  end
+end
