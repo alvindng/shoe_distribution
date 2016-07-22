29c60fea908a325690cef32d653628826a017bee
Alvin Ng <alvindng@gmail.com>
2016-07-22 11:24:32 -0700

    add passing spec for displaying brands for a store

--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,4 @@
 class Store < ActiveRecord::Base
-  has_and_belongs_to_many (:brands)
+  has_and_belongs_to_many(:brands)
 
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,1 +1,12 @@
 require('spec_helper')
+
+describe Store do
+  describe('brands') do
+    it "returns brands associated with a store" do
+      test_store = Store.create({:name => 'Champs'})
+      test_brand = Brand.create({:name => 'Nike'})
+      test_store.brands.push(test_brand)
+      expect(test_store.brands).to(eq([test_brand]))
+    end
+  end
+end
