744756be85dcb50afd1242c50e9387e52b8d320b
Alvin Ng <alvindng@gmail.com>
2016-07-22 11:25:47 -0700

    add passing spec for displaying stores for a brand

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,1 +1,12 @@
 require('spec_helper')
+
+describe Brand do
+  describe('stores') do
+    it "returns stores associated with a store" do
+      test_store = Store.create({:name => 'Champs'})
+      test_brand = Brand.create({:name => 'Nike'})
+      test_brand.stores.push(test_store)
+      expect(test_brand.stores).to(eq([test_store]))
+    end
+  end
+end
