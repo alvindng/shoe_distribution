45c3f88d4eb7e6fd432a6616a5ef6c8bb3d0df84
Alvin Ng <alvindng@gmail.com>
2016-07-22 11:59:11 -0700

    add initial views pages and paths

---
+++ app.rb
@@ -0,0 +1,13 @@
+require("bundler/setup")
+Bundler.require(:default)
+require('pry')
+
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  erb(:index)
+end
+
+get('/shoes') do
+  erb(:stores)
+end
